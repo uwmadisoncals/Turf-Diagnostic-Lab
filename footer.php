c16fc08d771d07009af06a78405c41e37207cfb4
Dixie Lang <dixie.lang@ymail.com>
2017-01-27 18:09:33 -0600

    Tidy the HTML and add Kurt's name as the accessibility contact.

--- footer.php
+++ footer.php
@@ -18,7 +18,8 @@
 			<div class="site-info" style="text-align: center; font-size: .8em !important;">
 			
 			<br /><hr />
-<!-- Display logo(s) -->
+			
+<!-- Setup path to logo(s) -->
 <?php 
 $LogoPath = get_bloginfo('stylesheet_directory') .'/custom_footers/images/'; 
 echo 'The logo path is:'.$LogoPath.'<br />';
@@ -27,20 +28,23 @@ $UWEX_Logo = 'uwex-logo.png';
 ?>			
 <img src="<?php echo $LogoPath.$UW_Logo; ?>" alt="UW Logo" style="float: left; margin-left: auto; margin-right: auto; width: 181px; height: 62px;">	
 
+<!-- Display accessibility link and copyright info -->
 <div style="float: left; margin-left: auto; margin-right: auto; width: 64%; ">		
 	<a href="http://www.wisc.edu/">UW-Madison</a>  &nbsp; &nbsp; &nbsp;  <a href="http://www.cals.wisc.edu/">College of Agricultural and Life Sciences</a> <br /><a href="http://www.plantpath.wisc.edu/">UW-Madison Department of Plant Pathology</a><br /><br />	
 				
-	<span class="feedback-text">Accessibility questions: Click <a href="mailto:bschweiger@wisc.edu">here</a> to send an email</span><br /><br />
+	<span class="feedback-text">Accessibility questions: Click <a href="mailto:hockmeyer@wisc.edu">here</a> to send an email.</span> <br /><br />
 	<span style="color: black;">Copyright &copy; <?php
 	$now = date('Y');
-	echo $now; ?> The Board of Regents of the University of Wisconsin System </span><br /><br />
+	echo $now; ?> The Board of Regents of the University of Wisconsin System</span><br /><br />
 	
 </div>
 
+<!-- Display logo(s) -->
 <img src="<?php echo $LogoPath.$UWEX_Logo; ?>" alt="UWEX Logo" style="float: left; margin-left: auto; margin-right: auto; width: 155px; height: 47px;">	<br /><br /><br /><br /><br /><br />
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<!-- Display site login link -->
 	<?php wp_footer(); ?>
 <div class="login-link" style="font-size: 80%"><a href="<?php echo site_url().'/wp-admin';?>">Login</a></div>
 
